b7ecf6dcbc9ea03909709356f769451ee9f43e67
dmlaziuk <dm.laziuk@gmail.com>
2018-12-02 22:58:48 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const dev = 'development'
+// const prod = 'production'
+
+module.exports = (env, {
+  mode = dev
+}) => ({
+  mode,
+  entry: './index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: 'body',
+      hash: true
+    })
+  ]
+})
